379b4ccca6d4703deb89d74b27e60ac0d282176f
kliu33 <kliu33@buffalo.edu>
2022-11-23 15:16:19 -0500

    up to bsearch

--- recursion.rb
+++ recursion.rb
@@ -25,3 +25,32 @@ def fibonacci(n)
     prev_fibs << (prev_fibs[-2] + prev_fibs[-1]) 
     prev_fibs
 end
+
+def it_fib(n)
+    fib_arr = [0, 1]
+    i = 2
+    while i < n
+        fib_arr << fib_arr[i-2] + fib_arr[i-1]
+        i += 1
+    end
+    return fib_arr
+end
+
+def bsearch(arr, target)
+    if arr.length <= 1
+        return 0 if target == arr[0]
+        return "not found"
+    end
+    mid = (arr.length / 2).floor
+    return mid if arr[mid] == target
+    left = arr[0...mid]
+    right = arr[mid + 1..-1]
+    if target < arr[mid]    
+        return bsearch(left, target) 
+    else
+        right_bsearch = bsearch(right, target)
+        return right_bsearch if right_bsearch == "not found"
+        return right_bsearch + mid + 1
+    end
+end
+
